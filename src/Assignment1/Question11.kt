de5ffed506bb0960224cb867bf1b2dbd6d64d9b6
[Sulav Shrestha] <[Sulavstha34@gmail.com]>
2020-12-04 08:57:07 +0545

    Question11

---
+++ src/Assignment1/Question11.kt
@@ -0,0 +1,17 @@
+package Assignment1
+
+fun main(){
+    print("Enter the starting number: ")
+    val start: Int= readLine()!!.toInt()
+
+    print("Enter the ending number: ")
+    val end: Int= readLine()!!.toInt()
+
+    var sum:Int =0
+
+    for (start: Int in start..end)
+        if (start%2==0)
+            sum += start
+    print("$sum")
+
+}
